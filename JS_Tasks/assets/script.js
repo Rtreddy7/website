e4c6620d7f105f46fff01e15920a4d2611037d27
Rtreddy7 <tej.1653.ravi@gmail.com>
2025-11-06 17:08:43 +0530

    JS-basics-Task

---
+++ JS_Tasks/assets/script.js
@@ -0,0 +1,47 @@
+//Declaring variables using variables:
+
+var city = "Anantpur";
+const age = 20;
+let country = "India";
+
+city = "Tadipatri";
+//age = 30;
+// Since const value could'nt be re-assigned so we get error in the console 
+country = "Ireland";
+
+console.log("Country:", country);
+console.log("City:", city);
+console.log("Age:", age);
+
+
+//Declaring the variables using let and checking the o/p for re-assigning and re-declaring it:
+
+let age_x = 27;
+
+console.log("Age:", age_x);
+
+age_x = 54;
+
+console.log("Age:", age_x);
+
+//let age_x = 81;
+
+//console.log("Age:", age_x); 
+
+//If we re-declare any variable console will throw an error because the variable is already declared and we can re-assign the values accordingly.
+
+
+//Checking the o/p of the variable using var
+
+var name_x = "Ravi Teja Reddy";
+
+console.log("Name:", name_x);
+
+name_x = "Tej Reddy";
+
+console.log("Name:", name_x);
+
+
+
+
+
